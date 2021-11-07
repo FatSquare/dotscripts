eb018515c70f6f3fb178d0e14972757aba68086d
FatSquare <legitcsgo5@gmail.com>
2021-11-07 16:06:56 +0100

    js code..

---
+++ discord-mute-discord-servers/app.js
@@ -0,0 +1,11 @@
+lensrv = document.getElementsByClassName("wrapper-1BJsBx").length // length of servers
+for(var i = 1;i< lensrv;i++){  
+document.getElementsByClassName("wrapper-1BJsBx")[i].dispatchEvent(new MouseEvent('contextmenu', {'bubbles': true})); // right click the server
+var mute1 = document.getElementsByClassName("label-22pbtT")[1]
+var mute2 = document.getElementsByClassName("label-22pbtT")[2]
+if(mute1.innerHTML == "Mute server"){
+mute1.click()//if first element is the mute-b
+}else if(mute2.innerHTML == "Mute server"){
+mute2.click()//if second element is the mute-b
+}
+}
