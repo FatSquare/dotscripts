c31c9af174de5130d8b0272bb38a1736b6bda6c4
FatSquare <legitcsgo5@gmail.com>
2021-11-07 17:18:37 +0100

    new discord version

--- discord-mute-servers/app.js
+++ discord-mute-servers/app.js
@@ -1,11 +1,14 @@
-lensrv = document.getElementsByClassName("wrapper-1BJsBx").length // length of servers
+var lensrv = document.getElementsByClassName("wrapper-1BJsBx").length
 for(var i = 1;i< lensrv;i++){  
-document.getElementsByClassName("wrapper-1BJsBx")[i].dispatchEvent(new MouseEvent('contextmenu', {'bubbles': true})); // right click the server
-var mute1 = document.getElementsByClassName("label-22pbtT")[1]
-var mute2 = document.getElementsByClassName("label-22pbtT")[2]
-if(mute1.innerHTML == "Mute server"){
-mute1.click()//if first element is the mute-b
-}else if(mute2.innerHTML == "Mute server"){
-mute2.click()//if second element is the mute-b
-}
+  document.getElementsByClassName("wrapper-1BJsBx")[i].dispatchEvent(new MouseEvent('contextmenu', {'bubbles': true}));
+  var mute1 = document.getElementsByClassName("label-22pbtT")[1]
+  var mute2 = document.getElementsByClassName("label-22pbtT")[2]
+  if(mute1.innerHTML.toLowerCase() == "mute server"){
+    mute1.click()
+    console.log(i)
+  }
+  else if(mute2.innerHTML.toLowerCase() == "mute server"){
+      mute2.click()
+      console.log(i)
+  }
 }
